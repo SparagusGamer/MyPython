3a1014ce12fedcc77f5c511696a0ec2c57aa0bbc
SparagusGamer <smartpixel11@gmail.com>
2021-12-12 14:29:37 +0100

    Create something.py

---
+++ something.py
@@ -0,0 +1,11 @@
+var = 'Python for Beginners'
+# Number of letters
+print(len(var))
+# All letters are in uppercase 
+print(var.upper())
+# All letters are lowercase
+print(var.lower())
+# All letters are in the default case 
+print(var)
+# the rest of the code has not been completed yet \/
+print(var.find('o'))
