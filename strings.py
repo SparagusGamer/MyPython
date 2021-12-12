4bbe505838a5f905bfb5610fb98d6422fb2facfe
SparagusGamer <smartpixel11@gmail.com>
2021-12-12 14:02:53 +0100

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,27 @@
+# Example 1
+example1 = "Emily's house"
+print(example1)
+# Example 2
+example2 = 'I like "The Hobbit" book'
+print(example2)
+# Example 3
+example3 = '''
+Hi John,
+
+Here is our first email to you.
+
+Thank you,
+The support team
+
+'''
+print(example3)
+# Example 4
+example4 = 'Python for Beginners'
+print(example4[0])
+print(example4[-1])
+print(example4[0:3])
+print(example4[0:])
+print(example4[1:])
+print(example4[:5])
+another = example4[:]
+print(another)
